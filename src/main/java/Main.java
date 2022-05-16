ac45b7743e8b448810987f141ccf7daf0da59485
Duy <dqduy231001@gmail.com>
2022-05-16 20:21:13 +0700

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("helllo");
+    }
+    public int sum(int a, int b,int c){
+        return a+b+c;
+    }
+}
